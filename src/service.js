45f51b1a4325b6606907f81b815d0a74cd682933
Juan Hinojosa <juanezequielhinojosa@gmail.com>
2022-10-11 18:59:05 -0300

    se agrego el servicio que servira para hacer la consulta a la API

---
+++ src/service.js
@@ -0,0 +1,10 @@
+export const getClima = async (latitud,longitud) => {
+  try {
+
+   const response = await fetch(`https://api.open-meteo.com/v1/forecast?current_weather=true&latitude=${latitud}&longitude=${longitud}&timezone=America/Argentina/Jujuy`);
+   return response.json();
+    
+  } catch {
+    throw new Error('could not fetch color palettes');
+  }
+};
