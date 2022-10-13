f3d37dccdc5a293f1ae0990c096a73038fef22f9
Juan Hinojosa <juanezequielhinojosa@gmail.com>
2022-10-13 17:13:58 -0300

    se trabajo con los estilos

--- src/routes/Clima/ClimaDisplay.jsx
+++ src/routes/Clima/ClimaDisplay.jsx
@@ -1,25 +1,47 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { UbicacionesContext } from "../../contexts/UbicacionesContext";
+import { getClima } from "../../service";
 import "./ClimaDisplay.css";
 
 //ESTE COMPONENTE PERMITE VISUALIZAR DE FORMA MAS DETALLADA EL CLIMA DE UNA UBICACION
 const ClimaDisplay = () => {
+  const [datos, setDatos] = useState({});
+  const [items, setItems] = useState({});
+
   const { id } = useParams();
   const { listaUbicaciones } = useContext(UbicacionesContext);
-  const [u] = listaUbicaciones.filter((ubicacion) => ubicacion.id === Number(id));
-
+  const [u] = listaUbicaciones.filter(
+    (ubicacion) => ubicacion.id === Number(id)
+  );
+  //AQUI SE HACE LA LLAMADA A LA API
+  useEffect(() => {
+    getClima(u.latitud, u.longitud)
+      .then((data) => {
+        setItems(data);
+        setDatos(data.current_weather);
+      })
+      .catch((err) => console.log(err));
+  }, [u]);
+  console.log(datos);
   return (
-    <div className="palette-display-container">
-      <div className="palette-display-card">
+    <div className="contenedor-principal">
       <img src={u.imagen} alt="" />
-        <h1 className="palette-display-name">{u.name}</h1>
-        <h1 className="palette-display-name">{u.latitud}</h1>
-        <div className="tag-d-container"></div>
+
+      <div className="contendor-texto">
+        <div className="contendor-datos">
+          <h1 className="nombre-ciudad">
+            <strong>{u.name}</strong>
+          </h1>
+          <p>Temp: {datos.temperature} °C</p>
+          <p>Vel. viento: {datos.windspeed} Km/h</p>
+          <p>Long:{items.latitude}</p>
+          <p> "Lat: {items.longitude}" </p>
+        </div>
+        <Link className="btn-back" to="/">
+          Volver
+        </Link>
       </div>
-      <Link className="btn-back" to="/">
-        Volver a la Pagina Principal
-      </Link>
     </div>
   );
 };
